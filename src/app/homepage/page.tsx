f7bb54551b642ff1c963d535cc4eb89c4ad04a75
Eduardo <eduangelinicarvalho@gmail.com>
2023-07-14 12:59:33 -0300

    commit#3-2 more sections

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -16,6 +16,9 @@ import views from '../../../public/views.svg'
 import like from '../../../public/like.svg'
 import instagram from '../../../public/instagram-logo.svg'
 import tiktoklogo from '../../../public/tiktok-logo.svg'
+import Brain from '@/components/Icons/Brain'
+import Positive from '@/components/Icons/Positive'
+import Focus from '@/components/Icons/Focus'
 
 const Home = () => {
   return (
@@ -41,7 +44,8 @@ const Home = () => {
             <div className='lg:w-7/12'>
             </div>
             <div className='flex flex-col items-start gap-y-6  rounded-md  bg-black bg-opacity-30 max-lg:p-4 max-lg:m-4 lg:w-5/12 lg:p-6'>
-              <h1 className='text-5xl font-bold'>A ansiedade não precisa definir sua vida!</h1>
+              <h1 className='text-4xl font-bold'>Me chamo Bárbara, sou <span>PSICÓLOGA.</span></h1>
+              <h2 className='text-2xl font-bold'>A ansiedade não precisa definir sua vida!</h2>
               <p className='text-lg font-medium'>Ter liberdade da ansiedade é uma realidade! É possível ter uma vida mais leve aprendendo habilidades simples. Você pode aprender algumas delas agora mesmo!</p>
               <button className='border-none bg-blue-500 text-white p-2 rounded-md font-medium hover:bg-blue-700'>
                 Quero aprender como !
@@ -86,7 +90,44 @@ const Home = () => {
         </section>
 
         <section className='lg:my-10 lg:mx-20'>
-          <h2 className='text-3xl max-sm:text-xl max-sm:mx-8 font-bold text-center mb-8'>UM POUCO SOBRE MIM</h2>
+          <div className='mb-8 flex flex-col gap-y-10 items-center'>
+            <h3 className='text-3xl max-sm:text-2xl max-sm:mx-8 font-bold text-center'>MINHAS ABORDAGEMS TERAPÊUTICAS</h3>
+
+            <div className='flex justify-center gap-x-4 max-sm:flex-col max-lg:mx-3'>
+              <div className='border-2 border-blue-300 max-sm:m-3 lg:w-3/12 rounded-md'>
+                <div className='p-3 flex text-white items-center bg-blue-300 rounded-sm gap-x-5 justify-center'>
+                  <div className='rounded-full p-5 bg-white'>
+                    <Brain />
+                  </div>
+                  <div>
+                    <p className='font-bold'>Terapia cognitiva</p>
+                    <p className='font-bold'>comportamental</p>
+                  </div>
+                </div>
+                <p className='px-5 py-2'>É a teoria principal que guia minha visão de homem e relação com o ambiente.</p>
+              </div>
+              <div className='border-2 border-blue-300 max-sm:m-3 lg:w-3/12 rounded-md'>
+                <div className='p-3 flex text-white items-center bg-blue-300 rounded-sm gap-x-5 justify-center'>
+                  <div className='rounded-full p-5 bg-white'>
+                    <Positive />
+                  </div>
+                  <p className='font-bold'>Psicologia Positiva</p>
+                </div>
+                <p className='px-5 py-2'>É a teoria e o estudo das emoções positivas e da importância delas na manutenção da saúde. As pesquisas nesse campo nos trazem respostas de como promover saúde mental e bem-estar.</p>
+              </div>
+              <div className='border-2 border-blue-300 max-sm:m-3 lg:w-3/12 rounded-md'>
+                <div className='p-3 flex text-white items-center bg-blue-300 rounded-sm gap-x-5 justify-center'>
+                  <div className='rounded-full p-5 bg-white'>
+                    <Focus />
+                  </div>
+                  <p className='font-bold'>Atenção Plena</p>
+                </div>
+                <p className='px-5 py-2'>A incorporação de ferramentas e conceitos da atenção plena é um dos diferenciais da minha prática clínica e tem trazido resultados muito satisfatórios aos meus clientes por oferecer ferramentas práticas.</p>
+              </div>
+            </div>
+          </div>
+
+          <h2 className='text-3xl max-sm:text-2xl max-sm:mx-8 font-bold text-center mb-8'>UM POUCO SOBRE MIM</h2>
           <div className='flex items-center justify-center gap-x-10'>
             <div className='lg:w-6/12 max-lg:hidden'>
               <Image src={hero} alt="Logo" className='rounded-lg' />
@@ -149,6 +190,25 @@ const Home = () => {
             </div>
           </div>
         </section>
+
+        <section>
+          <div className='bg-blue-300 w-screen flex max-lg:flex-col justify-center max-lg:text-center items-center py-40 text-white'>
+            <div className='flex flex-col lg:items-start gap-y-3 xl:me-56 lg:me-16 max-lg:mb-20'>
+              <h2 className='text-4xl max-sm:mx-8 font-bold text-center '>Entre em contato</h2>
+              <p className='text-xl max-sm:mx-8 font-medium'>Para saber mais sobre o meu trabalho, vai ser um prazer ajuda-ló.</p>
+            </div>
+            <div>
+              <button className='border-none bg-blue-500 text-white p-4 rounded-md font-medium hover:bg-blue-700'>
+                Entrar em contato
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-3xl max-sm:text-xl max-sm:mx-8 font-bold text-center mb-8'>PERGUNTAS FREQUENTES</h2>
+        </section>
+
 
       </main>
 
